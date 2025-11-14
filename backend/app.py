0d9c0bddbb78ad14a30e309a61e057f2c15e289f
msuud <msuudoct2@gmail.com>
2025-11-14 14:43:43 +0530

    Fixed app.py

--- backend/app.py
+++ backend/app.py
@@ -367,4 +367,4 @@ if __name__ == '__main__':
         except OSError:
              pass 
             
-    app.run(debug=True, port=5000)
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
